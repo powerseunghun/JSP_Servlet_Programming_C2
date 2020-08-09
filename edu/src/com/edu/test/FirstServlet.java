2c1d7df48c2bee85f86f87b95f0a81a4f805f171
powerseunghun <size411@naver.com>
2020-08-09 20:08:12 +0900

    Servlet Implementation & execute

--- edu/src/com/edu/test/FirstServlet.java
+++ edu/src/com/edu/test/FirstServlet.java
@@ -2,12 +2,23 @@ package com.edu.test;
 
 import java.io.IOException;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+@WebServlet("/hello2")
+// Annotation Attribute
+// description : input servlet description
+// displayNames : external display Servlet name
+// initParmas : @WebInitParam Annotation's add
+// largeIcon : Large Icon Position . use Servlet
+// loadOnStartup : sequence .. to load Servlet in Container
+// name : Servlet name
+// smallicon : small Icon Position . use Servlet
+// urlPatterns : URL Pattern to this Servlet Call
+// value : looks like urlPatterns, Attribute name omission possible
 public class FirstServlet extends HttpServlet{
 	
 	// when first servlet call
@@ -16,9 +27,14 @@ public class FirstServlet extends HttpServlet{
 		System.out.println("init() execute!");
 	}
 	
-	// when Servlet EveryTime execute request... 
+	// when Servlet EveryTime execute request...
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 		System.out.println("service() execute!");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("destroy() execute!");
 	}
 }
