2f7d0cbb7808b58e1303a9f692e89d8730fe6d54
powerseunghun <size411@naver.com>
2020-08-19 00:42:12 +0900

    Share Request Unit Information

---
+++ edu/src/com/edu/test/BookTest2Servlet.java
@@ -0,0 +1,27 @@
+package com.edu.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/bookOutput")
+public class BookTest2Servlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		
+		Book book = (Book) req.getAttribute("book");
+		
+		out.print("<h3>Name : " + book.getTitle() + "</h3>");
+		out.print("<h3>Author : " + book.getAuthor() + "</h3>");
+		out.print("<h3>Publisher : " + book.getPublisher() + "</h3>");
+		
+		out.close();
+	}
+}
